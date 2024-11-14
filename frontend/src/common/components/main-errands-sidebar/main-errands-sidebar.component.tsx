7c4ef9202d28ec59a21bd0b2a0b215e87288dbb5
Henrik Sandström <henrik.sandstrom@sundsvall.se>
2024-11-14 11:35:10 +0100

    Remove unused feature flag

--- frontend/src/common/components/main-errands-sidebar/main-errands-sidebar.component.tsx
+++ frontend/src/common/components/main-errands-sidebar/main-errands-sidebar.component.tsx
@@ -22,7 +22,6 @@ import { useState } from 'react';
 import { CaseDataFilter } from '@casedata/components/casedata-filtering/casedata-filtering.component';
 import { CaseStatusValues } from '@casedata/components/casedata-filtering/components/casedata-filter-status.component';
 import { CasedataFilterSidebarStatusSelector } from '@casedata/components/casedata-filtering/components/casedata-filter-sidebarstatus-selector.component';
-import { isNotificationsEnabled } from '@common/services/feature-flag-service';
 
 export const MainErrandsSidebar: React.FC<{
   showAttestationTable;
@@ -73,9 +72,7 @@ export const MainErrandsSidebar: React.FC<{
               {user.firstName} {user.lastName}
             </span>
           </div>
-          {isNotificationsEnabled() ? (
-            <NotificationsBell toggleShow={() => setShowNotifications(!showNotifications)} />
-          ) : null}
+          <NotificationsBell toggleShow={() => setShowNotifications(!showNotifications)} />
         </div>
         <Divider />
         <div className="flex flex-col gap-8 py-24">
@@ -115,9 +112,7 @@ export const MainErrandsSidebar: React.FC<{
           </>
         )}
       </div>
-      {isNotificationsEnabled() ? (
-        <NotificationsWrapper show={showNotifications} setShow={setShowNotifications} />
-      ) : null}
+      <NotificationsWrapper show={showNotifications} setShow={setShowNotifications} />
     </aside>
   );
 };
--- frontend/src/common/services/feature-flag-service.ts
+++ frontend/src/common/services/feature-flag-service.ts
@@ -1,4 +1,3 @@
 import { isKC, isLOP } from './application-service';
 
-export const isNotificationsEnabled = () => true;
 export const isSuspendEnabled = () => isKC() || isLOP();
