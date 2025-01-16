700b36e4ec896518a105b9ef01ae1aa0d8abe834
Borgström Oliver <oliver.borgstrom@sundsvall.se>
2025-01-16 13:24:37 +0100

    changed casing and cleanup

--- frontend/src/casedata/services/contract-render-service.tsx
+++ frontend/src/casedata/services/contract-render-service.tsx
@@ -2,47 +2,8 @@ import React, { useEffect } from 'react';
 import { Table, Checkbox, FormControl, FormLabel, Input, DatePicker } from '@sk-web-gui/react';
 
 function renderContractTermCheckboxList({ getValues, setValue, register }) {
-  const renderRow = ({ key, header, conditionText, date, extraField }) => {
+  const RenderRow = ({ key, header, conditionText, date, extraField }) => {
     const isChecked = !!getValues(key);
-    /* 
-    <Table.Row key={key}>
-      <Table.Column className="flex flex-col items-start">
-        <FormControl className="my-md" data-cy={`${key}-checkbox`}>
-          <Checkbox
-            defaultChecked={!!getValues(key)}
-            onChange={(val) => {
-              setValue(
-                key,
-                val.target.checked
-                  ? {
-                      header,
-                      conditionText,
-                    }
-                  : undefined
-              );
-            }}
-          >
-            {conditionText ? <strong>{header}</strong> : header}
-          </Checkbox>
-          {key === 'markfororeningarTerms.condition.testDone' && getValues(key) ? (
-            <>
-              <strong>Ange Datum för provtagning</strong>
-              <DatePicker
-                onChange={(date) => setValue('markfororeningarTerms.condition.testDone.date', date)}
-                {...register('markfororeningarTerms.condition.testDone.date')}
-              ></DatePicker>
-            </>
-          ) : null}
-          <span>{conditionText}</span>
-        </FormControl>
-        {extraField ? (
-          <FormControl className="mb-md">
-            <FormLabel>{extraField.header}</FormLabel>
-            <Input type="number" {...register(extraField.key)} placeholder={extraField.placeholder} />
-          </FormControl>
-        ) : null}
-      </Table.Column>
-    </Table.Row> */
 
     useEffect(() => {
       if (isChecked) {
@@ -94,7 +55,7 @@ function renderContractTermCheckboxList({ getValues, setValue, register }) {
       </Table.Row>
     );
   };
-  return renderRow;
+  return RenderRow;
 }
 
 export default renderContractTermCheckboxList;
