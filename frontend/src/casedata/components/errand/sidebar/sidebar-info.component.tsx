e667b26d29bcc2bf34a35ae407f7aba8a37d3c34
Henrik Sandström <henrik.sandstrom@sundsvall.se>
2025-01-29 17:40:26 +0100

    Hide Avsluta button for phase Uppföljning

--- frontend/src/casedata/components/errand/sidebar/sidebar-info.component.tsx
+++ frontend/src/casedata/components/errand/sidebar/sidebar-info.component.tsx
@@ -537,31 +537,33 @@ export const SidebarInfo: React.FC<{}> = () => {
         )
       )}
 
-      {uiPhase !== UiPhase.slutfor && errand.phase !== ErrandPhase.verkstalla && (
-        <>
-          <Button
-            className="mt-16"
-            color="primary"
-            variant="secondary"
-            onClick={() => {
-              setModalIsOpen(true);
-              setCauseIsEmpty(false);
-            }}
-            disabled={
-              !(
-                uiPhase === UiPhase.granskning ||
-                uiPhase === UiPhase.utredning ||
-                uiPhase === UiPhase.beslut ||
-                uiPhase === UiPhase.uppfoljning
-              ) ||
-              !isErrandAdmin(errand, user) ||
-              isErrandLocked(errand)
-            }
-          >
-            Avsluta ärendet
-          </Button>
-        </>
-      )}
+      {uiPhase !== UiPhase.slutfor &&
+        errand.phase !== ErrandPhase.verkstalla &&
+        errand.phase !== ErrandPhase.uppfoljning && (
+          <>
+            <Button
+              className="mt-16"
+              color="primary"
+              variant="secondary"
+              onClick={() => {
+                setModalIsOpen(true);
+                setCauseIsEmpty(false);
+              }}
+              disabled={
+                !(
+                  uiPhase === UiPhase.granskning ||
+                  uiPhase === UiPhase.utredning ||
+                  uiPhase === UiPhase.beslut ||
+                  uiPhase === UiPhase.uppfoljning
+                ) ||
+                !isErrandAdmin(errand, user) ||
+                isErrandLocked(errand)
+              }
+            >
+              Avsluta ärendet
+            </Button>
+          </>
+        )}
 
       <Modal
         label="Avsluta ärendet"
