e2208b6663cc50a4615cff33ed4493a1d25d27ed
Henrik Sandström <henrik.sandstrom@sundsvall.se>
2025-01-29 11:50:29 +0100

    Adjust to fixed typos in API

--- frontend/src/casedata/interfaces/case-label.ts
+++ frontend/src/casedata/interfaces/case-label.ts
@@ -6,7 +6,7 @@ export enum PTCaseLabel {
 }
 
 export enum MEXCaseLabel {
-  'MEX_APPLICATION_SQUARE_PLACE' = 'Ansökan torgplats',
+  'MEX_SQUARE_PLACE' = 'Ansökan torgplats',
   'MEX_REQUEST_FOR_PUBLIC_DOCUMENT' = 'Begäran om allmän handling',
   'MEX_INVOICE' = 'Faktura',
   'MEX_LEASE_REQUEST' = 'Förfrågan arrende',
@@ -15,13 +15,13 @@ export enum MEXCaseLabel {
   'MEX_LAND_SURVEYING_OFFICE' = 'Lantmäteriförrättning',
   'MEX_LAND_INSTRUCTION' = 'Markanvisning',
   'MEX_UNAUTHORIZED_RESIDENCE' = 'Otillåten bosättning',
-  'MEX_REFERRAL_BUILDING_PERMIT_EARLY_DIALOUGE_PLANNING_NOTICE' = 'Remiss/Bygglov/Tidig dialog planbesked',
+  'MEX_REFERRAL_BUILDING_PERMIT_EARLY_DIALOGUE_PLANNING_NOTICE' = 'Remiss/Bygglov/Tidig dialog planbesked',
   'MEX_PROTECTIVE_HUNTING' = 'Skyddsjakt',
   'MEX_SELL_LAND_TO_THE_MUNICIPALITY' = 'Sälja mark till kommunen',
   'MEX_EARLY_DIALOG_PLAN_NOTIFICATION' = 'Tidig dialog planbesked',
   'MEX_LAND_RIGHT' = 'Tomträtt',
   'MEX_TERMINATION_OF_LEASE' = 'Uppsägning arrende',
-  'MEX_TERMINATION_OF_HUNTING_LEASE' = 'Uppsägning jakträtt',
+  'MEX_HUNTING_LEASE' = 'Uppsägning jakträtt',
   'MEX_APPLICATION_FOR_ROAD_ALLOWANCE' = 'Vägbidrag',
   'MEX_FORWARDED_FROM_CONTACTSUNDSVALL' = 'Ärende från Kontakt Sundsvall',
   'MEX_OTHER' = 'Övrigt',
