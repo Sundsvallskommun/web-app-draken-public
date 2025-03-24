a6879a03c0dc84c63489b57f6a8587462dbe3861
Borgström Oliver <oliver.borgstrom@sundsvall.se>
2025-03-24 09:14:03 +0100

    removed unused flags from application-service

--- frontend/src/common/services/application-service.ts
+++ frontend/src/common/services/application-service.ts
@@ -12,27 +12,7 @@ export const isPT = () => process.env.NEXT_PUBLIC_APPLICATION === 'PT';
 
 export const isMEX = () => process.env.NEXT_PUBLIC_APPLICATION === 'MEX';
 
-export const isSupportManagement = () => isKC() || isIK() || isLOP();
-
-export const isCaseData = () => isPT() || isMEX();
-
 export const getApplicationEnvironment = () =>
   (isPT() || isKC() || isKA() || isIK() || isLOP() || isMEX()) && process.env.NEXT_PUBLIC_ENVIRONMENT === 'TEST'
     ? 'TEST'
     : null;
-
-export const usesThreeLevelCategorization = () => isLOP() || isIK();
-
-export const usesTwoLevelCategorization = () => isKC();
-
-export const usesExplanationOfTheCause = () => isKC();
-
-export const usesReasonForContact = () => isKC();
-
-export const usesBusinessCase = () => isKC();
-
-export const usesBilling = () => isLOP();
-
-export const usesFacilites = () => isMEX() || isKC();
-
-export const usesExtraInfomrationStakeholders = () => isMEX();
