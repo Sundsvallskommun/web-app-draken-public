13bd266cdb8dfd720e397882ed19544adaa1662a
ali18rin <alice.ringstrom@sundsvall.se>
2025-04-08 15:55:47 +0200

    feature: preparation for errand exports

---
+++ backend/src/controllers/export.controller.ts
@@ -0,0 +1,29 @@
+import { RequestWithUser } from '@/interfaces/auth.interface';
+import authMiddleware from '@/middlewares/auth.middleware';
+import ApiService from '@/services/api.service';
+import { Body, Controller, Param, Post, Req, Res, UseBefore } from 'routing-controllers';
+import { Errand } from '@/data-contracts/case-data/data-contracts';
+import { OpenAPI } from 'routing-controllers-openapi';
+
+/**
+ * Preparation for errand list export
+ */
+
+@Controller()
+export class ExportController {
+  private apiService = new ApiService();
+  private SERVICE = `endpoint/1.0`;
+
+  @Post('/:municipalityId/export')
+  @OpenAPI({ summary: 'Export list of errands' })
+  @UseBefore(authMiddleware)
+  async exportErrands(
+    @Req() req: RequestWithUser,
+    @Body() data: Errand[],
+    @Param('municipalityId') municipalityId: string,
+    @Res() response: string,
+  ): Promise<any> {
+    console.log('errands: ', data);
+    return true;
+  }
+}
