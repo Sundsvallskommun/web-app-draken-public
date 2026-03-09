baf8fe1e4aa5a2dfaf392b61b3ec3ccfc9b4b92b
Henrik Sandstrom <henrik.sandstrom@sundsvall.se>
2026-03-09 12:33:33 +0100

    feat: change display name for previous contract id field

--- frontend/src/casedata/components/errand/tabs/contract/contract-form.tsx
+++ frontend/src/casedata/components/errand/tabs/contract/contract-form.tsx
@@ -372,7 +372,7 @@ export const ContractForm: React.FC<{
             )}
             <div className="flex gap-18 justify-start">
               <FormControl id="oldContractId" className="w-full">
-                <FormLabel>Tidigare avtals-ID</FormLabel>
+                <FormLabel>Avtals-ID</FormLabel>
                 <Input
                   type="text"
                   data-cy="old-contract-id-input"
@@ -418,7 +418,9 @@ export const ContractForm: React.FC<{
                         ...(errandPropertyDesignations ?? []),
                         ...(existingContract?.propertyDesignations || []),
                       ];
-                      const selected = e.map((pdName) => totalPropertyDesignations.find((epd) => epd.name === pdName)).filter((pd): pd is { name: string; district?: string } => !!pd);
+                      const selected = e
+                        .map((pdName) => totalPropertyDesignations.find((epd) => epd.name === pdName))
+                        .filter((pd): pd is { name: string; district?: string } => !!pd);
                       setValue('propertyDesignations', selected);
                     }}
                   >
