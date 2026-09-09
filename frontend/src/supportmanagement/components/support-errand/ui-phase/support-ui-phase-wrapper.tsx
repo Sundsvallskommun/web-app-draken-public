e20b8bcc41f0ad7c3217f817f6de793840aecd66
Tobias Nordin <tobias.nordin@sundsvall.se>
2026-09-09 14:05:55 +0200

    feat(aot): updates ui-phase style

--- frontend/src/supportmanagement/components/support-errand/ui-phase/support-ui-phase-wrapper.tsx
+++ frontend/src/supportmanagement/components/support-errand/ui-phase/support-ui-phase-wrapper.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { Badge, cx } from '@sk-web-gui/react';
+import { Badge, cx, Icon } from '@sk-web-gui/react';
 import { useSupportStore } from '@stores/index';
 import {
   getSupportUiPhase,
   SUPPORT_UI_PHASE_ORDER,
   supportUiPhaseTranslationKey,
 } from '@supportmanagement/services/support-ui-phase-service';
+import { ArrowRight } from 'lucide-react';
 import { FC, Fragment } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -18,8 +19,8 @@ const SupportUiPhaseStep: FC<{ number: number; label: string; active: boolean; l
 }) => (
   <div className="flex items-center">
     <span className={cx(last ? 'pr-12' : null) + ' flex'}>
-      <div className="block ml-18">
-        <Badge rounded counter={number} color="vattjom" inverted={!active}></Badge>
+      <div className="block ml-12">
+        <Badge rounded={active} counter={number} color="vattjom" inverted={active}></Badge>
       </div>
       <div className={cx(active ? 'font-bold' : 'font-normal')}>
         <div className={cx(active ? null : 'hidden md:inline') + ' ml-8'}>{label}</div>
@@ -32,10 +33,14 @@ export const SupportUiPhaseWrapper = () => {
   const { t } = useTranslation();
   const supportErrand = useSupportStore((s) => s.supportErrand);
   const activePhase = getSupportUiPhase(supportErrand);
-  const arrow = <span className="border-t-2 border-r-2 h-[26px] w-[28px] rotate-45"></span>;
+  const arrow = (
+    <div className="flex pl-12">
+      <Icon icon={<ArrowRight />} size="1.5rem" />
+    </div>
+  );
 
   return (
-    <div className="flex items-center border-2 rounded-button h-[40px] w-fit">
+    <div className="flex items-center rounded-xl border-1 h-[40px] w-fit">
       {SUPPORT_UI_PHASE_ORDER.map((phase, index) => (
         <Fragment key={phase}>
           {index > 0 ? arrow : null}
